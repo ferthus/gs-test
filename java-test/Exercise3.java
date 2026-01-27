5c6c351fe6075414e4defe29ff2fa6e1a00c467d
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2026-01-27 10:15:32 -0600

    java: Adds solution to exercise 3

--- java-test/Exercise3.java
+++ java-test/Exercise3.java
@@ -6,3 +6,40 @@
  * input: {-3,-2,1,3,2,7}
  * output 4
  */
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		List<int[]> myList = Arrays.asList(new int[] {-3,-2,1,3,2,7});
+		
+		myList.forEach(l -> System.out.println("The smallest positive number which is not in the array " + 
+				Arrays.toString(l) + ", is: " + findSmallestPositiveNumber(l)));	
+	}
+	
+	public static int findSmallestPositiveNumber(int[] numbers) {
+		int resp = 0;
+		
+		Arrays.sort(numbers);
+		
+		int min = 0, index = 0;
+		
+		for(int i = 0; i < numbers.length; i++) {
+			if(numbers[i] > 0) {
+				min = numbers[i];
+				index = i;
+				break;
+			}
+		}		
+		
+		for(int i = min; i < numbers.length; i++) {
+			if(i != numbers[index++]) {
+				resp = i;
+				break;
+			}
+		}
+		
+		return resp;
+	}
+}
